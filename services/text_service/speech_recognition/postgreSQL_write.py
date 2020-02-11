239ba7072bbcb551e8b26e6791926645b6a5b6dd
MNIXenus <xenus150694@gmail.com>
2020-02-12 03:00:58 +1000

    add writing wrapper for postgresql DB

---
+++ services/text_service/speech_recognition/postgreSQL_write.py
@@ -0,0 +1,58 @@
+import psycopg2
+import keyring
+#keyring.set_password(service_name='#######', username='#####', password='######')
+#cursor.execute("DROP table text;")
+#cursor.execute("CREATE TABLE text (id bigserial PRIMARY KEY, work_place int, date_time timestamp without time zone, text text);")
+
+PG_DB_NAME = 'text'
+PG_USER = 'xenus'
+PG_SERVER_HOST = 'localhost'
+PG_SERVER_PORT = '5500'
+
+def write_row(work_place, text):
+    conn = psycopg2.connect(
+        host=PG_SERVER_HOST,
+        port=PG_SERVER_PORT,
+        dbname=PG_DB_NAME,
+        user=PG_USER,
+        password=keyring.get_password('postgreSQL', 'Xenus'),
+    )
+    cursor = conn.cursor()
+    cursor.execute(f"insert into text (work_place, date_time, text) VALUES ({work_place}, 'now', '{text}'); COMMIT;")
+    #cursor.execute("SELECT * FROM text;")
+    #print(cursor.fetchall())
+    cursor.close()
+    conn.close()
+
+write_row(1, 'wow that sucks')
+
+
+'''class connection(psycopg2.connect):
+    def __init__(self):
+        # noinspection PyArgumentList
+        self.cursor = super(
+                        
+                        ).cursor()
+    
+    def select_all_rows(self):
+        self.cursor.execute("SELECT * FROM text;")
+        print(self.cursor.fetchall())
+        self.cursor.close()
+    
+    def add_row(self, work_place, text):
+        self.
+        self.commit()
+    
+    def commit(self):
+        self.cursor.execute("COMMIT;")
+    self.cursor.close()
+    self.close()
+    
+cursor.execute()
+cursor.execute("COMMIT;")
+
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM text;")
+print(cursor.fetchall())
+cursor.close()
+conn.close()'''
