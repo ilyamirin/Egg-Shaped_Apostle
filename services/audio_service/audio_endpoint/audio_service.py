9e814f9a500789df9bf56c65768bb86221f02de9
MNIXenus <xenus150694@gmail.com>
2020-03-06 01:34:29 +1000

    fix endpoint service, add rolling deleting of files

--- services/audio_service/audio_endpoint/audio_service.py
+++ services/audio_service/audio_endpoint/audio_service.py
@@ -1,42 +1,85 @@
 import subprocess
-import ray
+import multiprocessing as mp
 from datetime import datetime
 import paramiko
 import configparser
 import keyring
+from time import sleep
+from os import remove, listdir
 
 #keyring.set_password('audio_service', 'pi', ########)
 config = configparser.ConfigParser()
 config.read('config.ini')
+START_HOUR = datetime.time(datetime.strptime('09:00', '%H:%M'))
+END_HOUR = datetime.time(datetime.strptime('18:00', '%H:%M'))
 
-ray.init()
+files_list = [config["ENV"]["DATA_DIR"]+i for i in listdir(config["ENV"]["DATA_DIR"])]
 
-@ray.remote
-def record(card, mic, time, file):
-    subprocess.call([r'/usr/bin/arecord', '-f', 'cd', '-D' f'plughw:{card},{mic}', '-d', f'{time}', f'{config["ENV"]["DATA_DIR"]+file}'])
-    return file
+def send_to_file_server(input_file, output_file):
+    global files_list
 
-def get_devices():
-    subprocess.call([r'/usr/bin/aplay', '-l'])
-
-def send_file(input_file, output_file):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(config['FILE_SERVER']['IP'], port=int(config['FILE_SERVER']['PORT']), username=config['FILE_SERVER']['USERNAME'], password=keyring.get_password('audio_service', 'pi'))
     sftp = ssh.open_sftp()
     sftp.put(input_file, output_file)
 
-#print(get_devices())
+    files_list.append(input_file)
+    if len(files_list) > 10:
+        remove(files_list[0])
+        files_list = files_list[1:]
+
+
+def record(q, card, mic, time, file):
+    subprocess.call([r'/usr/bin/arecord', '-f', 'cd', '-D' f'plughw:{card},{mic}', '-c', '1', '-d', f'{time}', f'{config["ENV"]["DATA_DIR"]+file}'])
+    q.put(file)
+
+
+def parallel_record(cards):
+    recording_processes = []
+    q = mp.Queue()
+    for card in cards:
+        for mic in cards[card]:
+            timestamp = str(datetime.now()).replace(' ', 'T')
+            recording_processes.append(
+                mp.Process(target=record, args=(q, card, mic, 10, f'{card}_{mic}_{timestamp}.wav')))
+
+    results = []
+
+    for i in recording_processes:
+        i.start()
+
+    for i in recording_processes:
+        results.append(q.get())
+
+    for i in recording_processes:
+        i.join()
+
+    return results
+
+
+def record_by_work_time(cards):
+    while True:
+        date_now = datetime.date(datetime.now())
+        start_datetime = datetime.combine(date_now, START_HOUR)
+        end_datetime = datetime.combine(date_now, END_HOUR)
+        start_delta = datetime.now().timestamp() - start_datetime.timestamp()
+        end_delta  = datetime.now().timestamp() - end_datetime.timestamp()
+        if start_delta < 0 and end_delta < 0:
+            results = parallel_record(cards)
+            for file in results:
+                send_to_file_server(config["ENV"]["DATA_DIR"]+file, config['FILE_SERVER']['DIR']+file)
+        else:
+            sleep(10)
+
+
 cards = {2: [0,], 3: [0,]}
 
-def record_work_day(cards):
-    while True:
-        recording_processes = []
+record_by_work_time(cards)
 
-        for card in cards:
-            for mic in cards[card]:
-                timestamp = str(datetime.now()).replace(' ', 'T')
-                recording_processes.append(record.remote(card, mic, 10, f'{card}_{mic}_{timestamp}'))
 
-        for file in ray.get(recording_processes):
-            send_file(config["ENV"]["DATA_DIR"]+file, config['FILE_SERVER']['DIR'])
+def get_devices():
+    subprocess.call([r'/usr/bin/aplay', '-l'])
+
+# print(get_devices())
+
