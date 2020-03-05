b1d41c01b8f271dbded05e6ef6abf752167b043c
MNIXenus <xenus150694@gmail.com>
2020-03-05 22:59:18 +1000

    add file sending and loop

--- services/audio_service/audio_endpoint/audio_service.py
+++ services/audio_service/audio_endpoint/audio_service.py
@@ -3,7 +3,9 @@ import ray
 from datetime import datetime
 import paramiko
 import configparser
+import keyring
 
+#keyring.set_password('audio_service', 'pi', ########)
 config = configparser.ConfigParser()
 config.read('config.ini')
 
@@ -20,14 +22,12 @@ def get_devices():
 def send_file(input_file, output_file):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(config['FILE_SERVER']['IP'], port=int(config['FILE_SERVER']['PORT']), username=config['FILE_SERVER']['USERNAME'], key_filename='id_rsa')
+    ssh.connect(config['FILE_SERVER']['IP'], port=int(config['FILE_SERVER']['PORT']), username=config['FILE_SERVER']['USERNAME'], password=keyring.get_password('audio_service', 'pi'))
     sftp = ssh.open_sftp()
     sftp.put(input_file, output_file)
 
 #print(get_devices())
 cards = {2: [0,], 3: [0,]}
-
-def send_file()
 
 def record_work_day(cards):
     while True:
@@ -39,4 +39,4 @@ def record_work_day(cards):
                 recording_processes.append(record.remote(card, mic, 10, f'{card}_{mic}_{timestamp}'))
 
         for file in ray.get(recording_processes):
-            send_file(config["ENV"]["DATA_DIR"]+file, )
+            send_file(config["ENV"]["DATA_DIR"]+file, config['FILE_SERVER']['DIR'])
